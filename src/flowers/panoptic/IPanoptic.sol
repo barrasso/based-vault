5603dd2840e2dcd080ea213f5b3a3fa3c3971baf
JaredBorders <jaredborders@pm.me>
2024-06-30 15:21:38 +0200

    🐝 sync

--- src/flowers/panoptic/IPanoptic.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
-
-interface IPanoptic {
-
-    function stake(uint256) external returns (uint256);
-    function unstake(uint256) external returns (uint256);
-
-}
